9cb38653bda0f5b4d7b97447aec294301bdefb2a
Jose Adolfo Galdamez <josegalre@gmail.com>
2017-10-15 04:52:02 -0600

    hosty: update installer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo curl -s https://raw.githubusercontent.com/JoseGalRe/Hosty/master/hosty -o /usr/local/bin/hosty && sudo chmod +x /usr/local/bin/hosty
-sudo curl -s https://raw.githubusercontent.com/JoseGalRe/Hosty/master/hosty.blacklist -o /usr/local/bin/hosty.blacklist
-sudo curl -s https://raw.githubusercontent.com/JoseGalRe/Hosty/master/hosty.whitelist -o /usr/local/bin/hosty.whitelist
+sudo curl -s https://raw.githubusercontent.com/JoseGalRe/Hosty/master/bits/hosty.blacklist -o /usr/local/bin/hosty.blacklist
+sudo curl -s https://raw.githubusercontent.com/JoseGalRe/Hosty/master/bits/hosty.whitelist -o /usr/local/bin/hosty.whitelist
